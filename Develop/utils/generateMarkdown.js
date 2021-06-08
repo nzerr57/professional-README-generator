307487ba9398098982915f841018c529776e9b0e
NZerr <nathan.zerr@gmail.com>
2021-06-08 19:20:04 -0400

    added license badge, link, and section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,56 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = (answers) => {
+  switch (answers) {
+    case 'MIT':
+      return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+      break;
+    case 'Apache 2.0':
+      return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
+      break;
+    case 'BSD 3-Clause':
+      return `![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)`
+      break;
+      case 'None':
+        return ""
+        break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = (answers) => { 
+  switch (answers) {
+  case 'MIT':
+    return `[MIT License](https://opensource.org/licenses/MIT)`
+    break;
+  case 'Apache 2.0':
+    return `[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)`
+    break;
+  case 'BSD 3-Clause':
+    return `[BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause)`
+    break;
+    case 'None':
+      return ""
+      break;
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseSection = (answers) => { 
+  if (answers !== 'None') {
+    return `## License`
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (answers) =>
   `# ${answers.title}
+
+${renderLicenseBadge(answers.license)}
 
   ## Description
   ${answers.description}
@@ -31,14 +69,14 @@ const generateMarkdown = (answers) =>
   ## Usage
   ${answers.usage}
 
-  ## License
-  ${answers.license}
+  ${renderLicenseSection(answers.license)}
+  ${renderLicenseLink(answers.license)}
 
   ## Contributors
   ${answers.contributors}
 
   ## Questions
-  If you have any questions please reach at github.com/${answers.github} or ${answers.email}
+  If you have any questions please reach out at github.com/${answers.github} or ${answers.email}
 `;
 
 module.exports = generateMarkdown;
